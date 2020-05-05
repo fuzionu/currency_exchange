666230671fbd6ad2fc8cdaf41ff4fa8dd2aff598
fuzionu <fuzionu@gmail.com>
2020-05-05 16:57:16 +0200

    added classes Exchange and ExchangeTest

---
+++ src/currency_exchange/Exchange.java
@@ -0,0 +1,9 @@
+package currency_exchange;
+
+public class Exchange
+{
+    public double exchangeCurrency(String from, String to, double amount)
+    {
+        return 0;
+    }
+}
---
+++ test/currency_exchange/ExchangeTest.java
@@ -0,0 +1,274 @@
+package currency_exchange;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.InputMismatchException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExchangeTest extends StandardInputTest
+{
+    @Test
+    void shouldExchangePlnToPln()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("pln", "pln", 1);
+
+        // then
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void shouldExchangePlnToEur()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("pln", "eur", 100);
+
+        // then
+        assertEquals(22.04, actual);
+    }
+
+    @Test
+    void shouldExchangePlnToUsd()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("pln", "usd", 120);
+
+        // then
+        assertEquals(28.74, actual);
+    }
+
+    @Test
+    void shouldExchangePlnToGbp()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("pln", "gbp", 30);
+
+        // then
+        assertEquals(5.76, actual);
+    }
+
+    @Test
+    void shouldExchangePlnToShkl()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("pln", "shkl", 69);
+
+        // then
+        assertEquals(58.01, actual);
+    }
+
+
+    @Test
+    void shouldExchangeEurToEur()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("eur", "eur", 1);
+
+        // then
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void shouldExchangeEurToPln()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("eur", "pln", 100);
+
+        // then
+        assertEquals(226.86, actual);
+    }
+
+    @Test
+    void shouldExchangeEurToUsd()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("eur", "usd", 38);
+
+        // then
+        assertEquals(41.29, actual);
+    }
+
+    @Test
+    void shouldExchangeEurToGbp()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("eur", "gbp", 23);
+
+        // then
+        assertEquals(20.02, actual);
+    }
+
+    @Test
+    void shouldExchangeEurToShkl()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("eur", "shkl", 27);
+
+        // then
+        assertEquals(103.08, actual);
+    }
+
+
+    @Test
+    void shouldExchangeUsdToUsd()
+    {
+        // given
+        input("exchange", "usd", "usd", "1", "exit");
+
+        // when
+        double actual = new Exchange().exchangeCurrency("usd", "usd", 1);
+
+        // then
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void shouldExchangeUsdToPln()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("usd", "pln", 115);
+
+        // then
+        assertEquals(480.06, actual);
+    }
+
+    @Test
+    void shouldExchangeUsdToEur()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("usd", "eur", 45);
+
+        // then
+        assertEquals(41.43, actual);
+    }
+
+    @Test
+    void shouldExchangeUsdToGbp()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("usd", "gbp", 19);
+
+        // then
+        assertEquals(15.26, actual);
+    }
+
+    @Test
+    void shouldExchangeUsdToShkl()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("usd", "shkl", 16);
+
+        // then
+        assertEquals(56.25, actual);
+    }
+
+
+    @Test
+    void shouldExchangeGbpToGbp()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("gbp", "gbp", 1);
+
+        // then
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void shouldExchangeGbpToPln()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("gbp", "pln", 13);
+
+        // then
+        assertEquals(67.67, actual);
+    }
+
+    @Test
+    void shouldExchangeGbpToEur()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("gbp", "eur", 31);
+
+        // then
+        assertEquals(35.61, actual);
+    }
+
+    @Test
+    void shouldExchangeGbpToUsd()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("gbp", "usd", 50);
+
+        // then
+        assertEquals(57.28, actual);
+    }
+
+    @Test
+    void shouldExchangeGbpToShkl()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("gbp", "shkl", 9);
+
+        // then
+        assertEquals(39.41, actual);
+    }
+
+    @Test
+    void shouldExchangeShklToShkl()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("shkl", "shkl", 1);
+
+        // then
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void shouldExchangeShklToPln()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("shkl", "pln", 99);
+
+        // then
+        assertEquals(117.6, actual);
+    }
+
+    @Test
+    void shouldExchangeShklToEur()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("shkl", "eur", 7);
+
+        // then
+        assertEquals(1.83, actual);
+    }
+
+    @Test
+    void shouldExchangeShklToUsd()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("shkl", "usd", 20);
+
+        // then
+        assertEquals(5.69, actual);
+    }
+
+
+    @Test
+    void shouldExchangeNegative()
+    {
+        // given
+        double actual = new Exchange().exchangeCurrency("eur", "pln", -100);
+
+        // then
+        assertEquals(-453.59, actual);
+    }
+
+    @Test
+    void shouldThrowInputMismatchException()
+    {
+        // given
+        assertThrows(InputMismatchException.class, () -> new Exchange().exchangeCurrency("xD", "pln", 5));
+    }
+}
