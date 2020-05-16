99793ba5d8046e84553c07ecca31d525286ae531
fuzionu <fuzionu@gmail.com>
2020-05-16 22:10:03 +0200

    added handling money equal one digit, two digits and zero, added tests

--- src/currency/exchanger/MoneyFormatter.java
+++ src/currency/exchanger/MoneyFormatter.java
@@ -7,8 +7,26 @@ public class MoneyFormatter
         String stringResult = Integer.toString(money);
         StringBuilder stringBuilder = new StringBuilder(stringResult);
 
-        stringBuilder.insert(stringResult.length() - 2, ".");
+        if (money == 0)
+        {
+            stringBuilder.insert(stringResult.length(), "00");
+            stringBuilder.insert(stringResult.length(), ".");
+            return stringBuilder.toString();
+        }
 
+        else if (money >= 10 && money < 100)
+        {
+            stringBuilder.insert(0, "0.");
+            return stringBuilder.toString();
+        }
+
+        else if (money < 10 && money > 0)
+        {
+            stringBuilder.insert(0, "0.0");
+            return stringBuilder.toString();
+        }
+
+        stringBuilder.insert(stringResult.length() - 2, ".");
         return stringBuilder.toString();
     }
 }
--- test/currency/exchanger/MoneyFormatterTest.java
+++ test/currency/exchanger/MoneyFormatterTest.java
@@ -7,15 +7,80 @@ import static org.junit.jupiter.api.Assertions.*;
 class MoneyFormatterTest
 {
     @Test
-    void shouldPutComaTwoSpaceFromEnd()
+    void shouldPutComaTwoSpaceFromEnd_BigAmount()
     {
         // given
-        int number = 254127;
+        int groshen = 254127;
 
         // when
-        String actual = MoneyFormatter.formatMoney(number);
+        String actual = MoneyFormatter.formatMoney(groshen);
 
         // then
         assertEquals("2541.27", actual);
+    }
+
+    @Test
+    void shouldPutComaTwoSpacesFromEnd_SmallAmount_OneDigit()
+    {
+        // given
+        int groshen = 8;
+
+        // when
+        String actual = MoneyFormatter.formatMoney(groshen);
+
+        // then
+        assertEquals("0.08", actual);
+    }
+
+    @Test
+    void shouldPutComaTwoSpacesFromEnd_SmallAmount_TwoDigits()
+    {
+        // given
+        int groshen = 10;
+
+        // when
+        String actual = MoneyFormatter.formatMoney(groshen);
+
+        // then    
+        assertEquals("0.10", actual);
+    }
+
+    @Test
+    void shouldPutComaTwoSpacesFromEnd_SmallAmount_ThreeDigits()
+    {
+        // given
+        int groshen = 153;
+
+        // when
+        String actual = MoneyFormatter.formatMoney(groshen);
+
+        // then
+        assertEquals("1.53", actual);
+    }
+
+    @Test
+    void shouldFormatZeroMoney()
+    {
+        // given
+        int groshen = 0;
+
+        // when
+        String actual = MoneyFormatter.formatMoney(groshen);
+
+        // then
+        assertEquals("0.00", actual);
+    }
+
+    @Test
+    void shouldFormatNegativeMoney()
+    {
+        // given
+        int groshen = -122;
+
+        // when
+        String actual = MoneyFormatter.formatMoney(groshen);
+
+        // then
+        assertEquals("-1.22", actual);
     }
 }
