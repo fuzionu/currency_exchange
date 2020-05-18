afaabf50223cbbe94a94e8a5c3d81ad735b74a16
fuzionu <fuzionu@gmail.com>
2020-05-18 23:28:07 +0200

    cleaned money format

--- src/main/java/currency/exchanger/MoneyParser.java
+++ src/main/java/currency/exchanger/MoneyParser.java
@@ -2,24 +2,30 @@ package currency.exchanger;
 
 import static java.lang.Integer.parseInt;
 
-public class MoneyParser {
-    public int parseMoney(String money) {
-        if (money.matches("^\\d+$")) {
-            return parse(money + "00");
-        }
-        if (money.matches("^\\d+[,.]?$")) {
+public class MoneyParser
+{
+    public int parseMoney(String money)
+    {
+        if (money.matches("^\\d+[,.]?$"))
+        {
             return parse(money.replaceAll("[.,]", "") + "00");
         }
-        if (money.matches("\\d*[.,](\\d\\d)?")) {
+
+        if (money.matches("\\d*[.,]\\d\\d"))
+        {
             return parse(money.replaceAll("[.,]", ""));
         }
+
         throw new InvalidMoneyFormatException();
     }
 
-    private int parse(String string) {
-        if (string.isEmpty()) {
+    private int parse(String string)
+    {
+        if (string.isEmpty())
+        {
             throw new InvalidMoneyFormatException();
         }
+
         return parseInt(string);
     }
 }
