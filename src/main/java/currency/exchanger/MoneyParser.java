f8d40f83c983e0a5ddec2ec57199bbbba74881d4
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-05-18 23:24:17 +0200

    Clean MoneyParser

--- src/main/java/currency/exchanger/MoneyParser.java
+++ src/main/java/currency/exchanger/MoneyParser.java
@@ -1,68 +1,25 @@
 package currency.exchanger;
 
-import java.util.ArrayList;
-import java.util.List;
+import static java.lang.Integer.parseInt;
 
-public class MoneyParser
-{
-    private final List<String> invalidMarks = getInvalidMarks();
-
-    public int parseMoney(String money)
-    {
-        try
-        {
-            return tryParseMoney(money);
+public class MoneyParser {
+    public int parseMoney(String money) {
+        if (money.matches("^\\d+$")) {
+            return parse(money + "00");
         }
-        catch (NumberFormatException exc)
-        {
+        if (money.matches("^\\d+[,.]?$")) {
+            return parse(money.replaceAll("[.,]", "") + "00");
+        }
+        if (money.matches("\\d*[.,](\\d\\d)?")) {
+            return parse(money.replaceAll("[.,]", ""));
+        }
+        throw new InvalidMoneyFormatException();
+    }
+
+    private int parse(String string) {
+        if (string.isEmpty()) {
             throw new InvalidMoneyFormatException();
         }
-    }
-
-    private int tryParseMoney(String money)
-    {
-        if (containsForbiddenMarks(money))
-        {
-            throw new InvalidMoneyFormatException();
-        }
-
-        if (!money.contains(".") && !money.contains(","))
-        {
-            return Integer.parseInt(money + "00");
-        }
-
-        if (money.substring(money.length() - 1).equals(".") || money.substring(money.length() - 1).equals(","))
-        {
-            return Integer.parseInt(money.replace(",", "").replace(".", "") + "00");
-        }
-
-        if (!validMoneyFormat(money))
-        {
-            throw new InvalidMoneyFormatException();
-        }
-
-        return Integer.parseInt(money.replace(",", "").replace(".", ""));
-    }
-
-    private boolean validMoneyFormat(String string)
-    {
-        return string.matches("\\d*([.,]\\d\\d)?");
-    }
-
-    private List<String> getInvalidMarks()
-    {
-        List<String> invalidMarks = new ArrayList<>();
-
-        invalidMarks.add(",");
-        invalidMarks.add(".");
-        invalidMarks.add("");
-        invalidMarks.add(" ");
-
-        return invalidMarks;
-    }
-
-    private boolean containsForbiddenMarks(String money)
-    {
-        return invalidMarks.contains(money);
+        return parseInt(string);
     }
 }
