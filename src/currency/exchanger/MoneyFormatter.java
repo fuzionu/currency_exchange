b62830adb8723280fddcaaeb8ffb76a32b202c08
fuzionu <fuzionu@gmail.com>
2020-05-18 16:57:22 +0200

    simplified money formatting

--- src/currency/exchanger/MoneyFormatter.java
+++ src/currency/exchanger/MoneyFormatter.java
@@ -4,29 +4,25 @@ public class MoneyFormatter
 {
     public static String formatMoney(int money)
     {
-        String stringResult = Integer.toString(money);
-        StringBuilder stringBuilder = new StringBuilder(stringResult);
-
         if (money == 0)
         {
-            stringBuilder.insert(stringResult.length(), "00");
-            stringBuilder.insert(stringResult.length(), ".");
-            return stringBuilder.toString();
+            return "0.00";
         }
 
         else if (money >= 10 && money < 100)
         {
-            stringBuilder.insert(0, "0.");
-            return stringBuilder.toString();
+            return "0." + money;
         }
 
         else if (money < 10 && money > 0)
         {
-            stringBuilder.insert(0, "0.0");
-            return stringBuilder.toString();
+            return "0.0" + money;
         }
 
+        String stringResult = Integer.toString(money);
+        StringBuilder stringBuilder = new StringBuilder(stringResult);
         stringBuilder.insert(stringResult.length() - 2, ".");
+
         return stringBuilder.toString();
     }
 }
