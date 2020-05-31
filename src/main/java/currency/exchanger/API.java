7646b78bc174bbb2ccc67eef3816fdb29961330b
fuzionu <fuzionu@gmail.com>
2020-05-31 23:34:53 +0200

    removed API interface, renamed class RealAPI to API, added generic types to map in API

--- src/main/java/currency/exchanger/API.java
+++ src/main/java/currency/exchanger/API.java
@@ -1,9 +1,16 @@
 package currency.exchanger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
+import java.net.URL;
 import java.util.Map;
 
-public interface API
+public class API
 {
-    Map getFactors() throws IOException;
+    public Map<Currency, Double> getFactors() throws IOException
+    {
+        Map map = new ObjectMapper().readValue(new URL("https://api.exchangeratesapi.io/latest"), Map.class);
+        return (Map) map.get("rates");
+    }
 }
